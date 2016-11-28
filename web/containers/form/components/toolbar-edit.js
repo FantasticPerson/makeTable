07cc6e0aadae552a61e8e85a8804f7ed991a9c34
dandan.wu <1240172019@qq.com>
2016-11-28 17:29:47 +0800

    toolbar 粗加工 step2

--- web/containers/form/components/toolbar-edit.js
+++ web/containers/form/components/toolbar-edit.js
@@ -7,32 +7,59 @@ import ToolBarEditDisplay from './toolbar-edit-display'
 export default class ToolBarEdit extends Component{
     constructor(){
         super();
+        this.state={cTool:null}
+    }
+
+    renderSubTool(){
+        const {cTool} = this.state;
+        if(cTool == 'display'){
+            return (<ToolBarEditDisplay/>);
+        }
+    }
+
+    onClickHandler(name){
+        const {cTool} = this.state;
+        let name2 = (cTool == name ? null : name);
+        this.setState({cTool:name2});
+    }
+
+    getStyleByName(name){
+        const {cTool} = this.state;
+        if(name == cTool){
+            return {color1:'#eef6fc',color2:'#6998d6'}
+        } else {
+            return {color1:'#FFFFFF',color2:'#000000'}
+        }
     }
 
     render(){
         const {} = this.props;
+        let styleElement = this.getStyleByName('element');
+        let styleDisplay = this.getStyleByName('display');
+        let stylePreview = this.getStyleByName('preview');
+        let styleSource = this.getStyleByName('source');
         return (
             <div className="true-form-tool-bar-edit-container">
                 <div className="true-form-edit-icon-container">
-                    <div className="true-form-edit-element-icon-container">
+                    <div className="true-form-edit-element-icon-container" style={{backgroundColor:styleElement.color1}} onClick={()=>{this.onClickHandler('element')}}>
                         <div className="true-form-edit-element-icon true-form-edit-icon-style"></div>
-                        <div className="true-form-edit-icon-style">{'部件'}</div>
+                        <div className="true-form-edit-icon-style" style={{color:styleElement.color2}}>{'部件'}</div>
                     </div>
-                    <div className="true-form-edit-display-icon-container">
+                    <div className="true-form-edit-display-icon-container" style={{backgroundColor:styleDisplay.color1}} onClick={()=>{this.onClickHandler('display')}}>
                         <div className="true-form-edit-display-icon true-form-edit-icon-style"></div>
-                        <div className="true-form-edit-icon-style">{'布局'}</div>
+                        <div className="true-form-edit-icon-style" style={{color:styleDisplay.color2}}>{'布局'}</div>
                     </div>
-                    <div className="true-form-edit-preview-icon-container">
+                    <div className="true-form-edit-preview-icon-container" style={{backgroundColor:stylePreview.color1}} onClick={()=>{this.onClickHandler('preview')}}>
                         <div className="true-form-edit-preview-icon true-form-edit-icon-style"></div>
-                        <div className="true-form-edit-icon-style">{'预览'}</div>
+                        <div className="true-form-edit-icon-style" style={{color:stylePreview.color2}}>{'预览'}</div>
                     </div>
-                    <div className="true-form-edit-source-icon-container">
+                    <div className="true-form-edit-source-icon-container" style={{backgroundColor:styleSource.color1}} onClick={()=>{this.onClickHandler('source')}}>
                         <div className="true-form-edit-source-icon true-form-edit-icon-style"></div>
-                        <div className="true-form-edit-icon-style">{'查看源代码'}</div>
+                        <div className="true-form-edit-icon-style" style={{color:styleSource.color2}}>{'查看源代码'}</div>
                     </div>
                 </div>
                 <div className="true-form-edit-icon-sub-container">
-                    <ToolBarEditDisplay/>
+                    {this.renderSubTool()}
                 </div>
             </div>
         )
--- web/containers/form/components/toolbar.js
+++ web/containers/form/components/toolbar.js
@@ -7,27 +7,51 @@ import ToolbarEdit from './toolbar-edit'
 export default class ToolBar extends Component{
     constructor(){
         super();
+        this.state = {subTool:null};
+    }
+
+    renderSubTool(){
+        const {subTool} = this.state;
+        if(subTool == 'edit'){
+            return (<ToolbarEdit/>)
+        }
+    }
+
+    onBtnClick(name){
+        const {subTool} = this.state;
+        let name2 = (subTool == name ? null : name);
+        this.setState({subTool:name2});
+    }
+
+    getStyleByName(name){
+        const {subTool} = this.state;
+        if(name == subTool){
+            return {color1:'#eef6fc',color2:'#6998d6'}
+        } else {
+            return {color1:'#FFFFFF',color2:'#000000'}
+        }
     }
 
     render(){
         const {style} = this.props;
+        let styleEdit = this.getStyleByName('edit');
+        let styleStyle = this.getStyleByName('style');
+        let styleTool = this.getStyleByName('tool');
         return(
             <div>
                 <div className="true-form-tool-bar-container" style={style}>
                     <div className="tool-bar-left" style={{marginLeft:'10px',display:'flex'}}>
-                        <div className="true-form-edit-edit-container">
+                        <div className="true-form-edit-edit-container" style={{backgroundColor:styleEdit.color1}} onClick={()=>{this.onBtnClick('edit')}}>
                             <div className="true-form-edit-edit-icon"></div>
-                            <div className="true-form-edit-edit-text">{'编辑'}</div>
-
+                            <div className="true-form-edit-edit-text" style={{color:styleEdit.color2}}>{'编辑'}</div>
                         </div>
-
-                        <div className="true-form-custom-style-icon-container">
+                        <div className="true-form-custom-style-icon-container" style={{backgroundColor:styleStyle.color1}} onClick={()=>{this.onBtnClick('style')}}>
                             <div className="true-form-custom-style-icon"></div>
-                            <div className="true-form-custom-style-text">{'自定义样式'}</div>
+                            <div className="true-form-custom-style-text" style={{color:styleStyle.color2}}>{'自定义样式'}</div>
                         </div>
-                        <div className="true-form-tool-icon-container">
+                        <div className="true-form-tool-icon-container" style={{backgroundColor:styleTool.color1}} onClick={()=>{this.onBtnClick('tool')}}>
                             <div className="true-form-tool-icon"></div>
-                            <div className="true-form-tool-text">{'工具'}</div>
+                            <div className="true-form-tool-text" style={{color:styleTool.color2}}>{'工具'}</div>
                         </div>
                     </div>
                     <div className="tool-bar-right" style={{display:'flex',marginRight:'30px'}}>
@@ -38,7 +62,7 @@ export default class ToolBar extends Component{
                     {this.props.children}
                 </div>
                 <div className="true-form-sub-tool-bar-container">
-                    <ToolbarEdit/>
+                    {this.renderSubTool()}
                 </div>
             </div>
         );
