92998936cf8dab2ea6f2a3bb5eccd1d7a32ffa05
dandan.wu <1240172019@qq.com>
2016-11-30 10:15:51 +0800

    update colorPicker

--- web/containers/form/components/colorPicker.js
+++ web/containers/form/components/colorPicker.js
@@ -2,27 +2,28 @@
  * Created by wdd on 2016/11/29.
  */
 import React,{Component,PropTypes} from 'react'
-import {SliderPicker} from 'react-color'
+import {HuePicker,AlphaPicker} from 'react-color'
 
 export default class ColorPicker extends Component{
     constructor(){
         super();
-        this.state = {color: {
-            r: '241',
-            g: '112',
-            b: '19',
-            a: '1',
-        }}
+        this.state = {color: {r: '241', g: '112', b: '19', a: '1'}}
     }
 
-    onChange(color,event){
+    onChange(color){
         this.setState({color:color.rgb});
     }
 
     render(){
+        const {color} = this.state;
+        let colorString = 'rgba('+color.r+','+color.g+','+color.b+','+color.a+')';
         return (
-            <div style={{width:'200px',height:'100px'}}>
-                <SliderPicker color={this.state.color} onChange={this.onChange.bind(this)}/>
+            <div style={{width:'300px'}}>
+                <HuePicker width={'300'} color={color} onChange={this.onChange.bind(this)}/>
+                <div style={{marginTop:'10px'}}>
+                    <AlphaPicker width={'300'} color={color} onChange={this.onChange.bind(this)}/>
+                </div>
+                <div style={{backgroundColor:colorString}} className="color-picker-color-cube"></div>
             </div>
         )
     }
