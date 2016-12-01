1000492a6b0e54a3d323a2bfcbe645d93b5041c1
dandan.wu <1240172019@qq.com>
2016-12-01 17:24:20 +0800

    添加  component 对象

--- web/containers/form/utils/checkBoxMaker.js
+++ web/containers/form/utils/checkBoxMaker.js
@@ -1,3 +1,31 @@
 /**
  * Created by wdd on 2016/11/30.
  */
+import React,{Component,PropTypes} from 'react'
+
+export default class TextMaker extends Object{
+    constructor(id,tdId,type,style,onComponentClick){
+        super();
+        this.tdId = tdId;
+        this.id = id;
+        this.type = 'checkBox';
+        this.style = style;
+        this.onContextMenu = onComponentClick;
+        this.getNode = getNode;
+        this.setStyle = setStyle;
+    }
+}
+
+export function setStyle(style){
+    this.style = style;
+}
+
+export function getNode(index){
+    return (
+        <input type="checkbox" style={{...this.style}} key={index} onClick={(e)=>{e.stopPropagation()}} onContextMenu={(e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+            this.onContextMenu({type:this.type,id:this.id,tdId:this.tdId,pageX:e.pageX,pageY:e.pageY});
+        }}/>
+    )
+}
