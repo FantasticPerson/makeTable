acfb03f1b0fde1d16a46d76084b4a975a01bbbcf
dandan.wu <1240172019@qq.com>
2017-01-11 09:16:23 +0800

    鼠标移动到 工具上面  显示提示框

--- web/containers/form/components/componentPicker.js
+++ web/containers/form/components/componentPicker.js
@@ -22,27 +22,34 @@ export default class ComponentPicker extends Component{
                 </div>
                 <div className="true-form-tool-bar-component-body-container">
                     <div className="true-form-tool-bar-component-body-line-1">
-                        <div className="true-form-tool-bar-component-outer" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'text')}}>
+                        <div className="true-form-tool-bar-component-outer true-form-tool-tip" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'text')}}>
                             <div className="true-for-tool-bar-component-text"></div>
+                            <span>{'纯文本'}</span>
                         </div>
-                        <div className="true-form-tool-bar-component-outer" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'input')}}>
+                        <div className="true-form-tool-bar-component-outer true-form-tool-tip" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'input')}}>
                             <div className="true-form-tool-bar-component-input"></div>
+                            <span>{'输入框'}</span>
                         </div>
                     </div>
                     <div className="true-form-tool-bar-component-body-line-2">
-                        <div className="true-form-tool-bar-component-outer" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'radioBox')}}>
+                        <div className="true-form-tool-bar-component-outer true-form-tool-tip" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'radioBox')}}>
                             <div className="true-form-tool-bar-component-radio"></div>
+                            <span>{'复选框'}</span>
                         </div>
-                        <div className="true-form-tool-bar-component-outer" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'checkBox')}}>
+                        <div className="true-form-tool-bar-component-outer true-form-tool-tip" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'checkBox')}}>
                             <div className="true-form-tool-bar-component-check-box"></div>
+                            <span>{'单选框'}</span>
                         </div>
+
                     </div>
                     <div className="true-form-tool-bar-component-body-line-3">
-                        <div className="true-form-tool-bar-component-outer" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'textArea')}}>
+                        <div className="true-form-tool-bar-component-outer true-form-tool-tip" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'textArea')}}>
                             <div className="true-form-tool-bar-component-text-area"></div>
+                            <span>{'输入区域'}</span>
                         </div>
-                        <div className="true-form-tool-bar-component-outer" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'dropBox')}}>
+                        <div className="true-form-tool-bar-component-outer true-form-tool-tip" draggable="true" onDragStart={(e)=>{this.onDragStart(e,'dropBox')}}>
                             <div className="true-form-tool-bar-component-drop-down"></div>
+                            <span>{'下拉框'}</span>
                         </div>
                     </div>
                 </div>
